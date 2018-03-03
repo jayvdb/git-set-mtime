d584f6a2afc7b8e07e22179c3cd93783d8d36f46
John Vandenberg <jayvdb@gmail.com>
2018-03-03 11:39:41 +0700

    mtime.rb: Check file exists

--- lib/git/set/mtime.rb
+++ lib/git/set/mtime.rb
@@ -12,11 +12,13 @@ module Git::Set::Mtime
       files = `git ls-files`
       files.each_line do |file|
         file.chomp!
-        mtime_str, status = Open3.capture2e(*GIT_LOG_ARGS, file)
-        raise mtime_str unless status.success?
-        mtime = Time.parse(mtime_str)
-        File.utime(File.atime(file), mtime, file)
-        puts "#{mtime} #{file}"
+        if File.exists? file
+          mtime_str, status = Open3.capture2e(*GIT_LOG_ARGS, file)
+          raise mtime_str unless status.success?
+          mtime = Time.parse(mtime_str)
+          File.utime(File.atime(file), mtime, file)
+          puts "#{mtime} #{file}"
+        end
       end
     end
 
